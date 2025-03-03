f01983557a3357b7cc5cf4fe1d389c1930caf9b1
snggooroid <>
2025-03-03 18:13:46 +0900

    test4

---
+++ src/main/java/com/example/cicd/AppController.java
@@ -0,0 +1,12 @@
+package com.example.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String home(){
+        return "hello world";
+    }
+}
